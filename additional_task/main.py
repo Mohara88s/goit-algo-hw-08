2946a187de5c6d166746e111332e47d51ef84804
Mohara88s <mohara88s@gmail.com>
2025-05-25 10:37:36 +0300

    make additional task

---
+++ additional_task/main.py
@@ -0,0 +1,8 @@
+import heapq
+
+def merge_k_lists(lists):
+    return list(heapq.merge(*lists))
+
+lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+merged_list = merge_k_lists(lists)
+print("Відсортований список:", merged_list)
